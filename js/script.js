43e3e79b7811173dc5ed370e93702347d9d63069
Frankosviz <geom.francescopaolo@gmail.com>
2024-04-01 01:52:58 +0200

    Slow code incrementation : click-on prev-next, methods next-prev Slide

--- js/script.js
+++ js/script.js
@@ -6,11 +6,21 @@ const {createApp} = Vue;
 createApp({
     data() {
         return {
+
+            // Associo la const slides con un array 
+            
             slides: slides,
+
+            // Creo una variabile activeIndexSlide e la inizializzo a 0
+            // Variabile che mi serve per tenere traccia dell'indice dell'elemento attivo nella lista slides. Inizializzo a 0 perchè all'avvio l'elemento attivo sarà il primo.
+            
             activeIndexSlide: 0
         }    
     },
     methods: {
+
+        // Creo due metodi che mi permettono di andare avanti e indietro sulle miniature
+        
         nextSlide(){
             this.activeIndexSlide++
         },
